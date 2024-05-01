5ad960034540297c5a10c47fd31c8b83c1f1ac25
Jessie Chen <jud40322@gmail.com>
2024-05-01 15:31:02 +0800

    fix : remove singer_info init()

--- script/model/singer_info.py
+++ script/model/singer_info.py
@@ -6,11 +6,9 @@ Base = declarative_base()
 class SingerInfo(Base):
     __tablename__ = 'singer_info'
     singer_info_id = Column(Integer, primary_key=True)
-    singer_info_name = Column(String(30), nullable=False)
+    singer_info_name = Column(String(300), nullable=False)
     singer_info_image_url = Column(String(1000), nullable=True)
-
-    def __init__(self, name):
-        self.singer_info_name = name
 
     def __repr__(self):
         return f"<singer_info(singer_info_id={self.singer_info_id}, singer_info_name={self.singer_info_name})>"
+    
